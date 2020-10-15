df7bb94579ed10bd29f80ce465e547f5724d25c8
Алмаз Залялов <zalyalov.almaz2016@gmail.com>
2020-10-15 13:48:35 +0300

    uploading homework 2

--- src/dz2/Cashback.java
+++ src/dz2/Cashback.java
@@ -1,33 +1,12 @@
 package dz2;
 
 public class Cashback {
-
-    public static int cashback;
-
-}
-
-class Purchase {
-    int summ;
-    private int cashbackPercent;
-
-    public Purchase(int summ, int cashbackPercent) {
-        this.summ = summ;
-        this.cashbackPercent = cashbackPercent;
-        Cashback.cashback = this.summ * this.cashbackPercent/100;
-        if (Cashback.cashback >= 3000) {
-            Cashback.cashback = 3000;
-        }
-    }
-}
-
-class Test {
-
     public static void main(String[] args) {
-        Purchase p = new Purchase(500, 1);
-        System.out.println(Cashback.cashback);
-        Purchase p2 = new Purchase(1500, 5);
-        System.out.println(Cashback.cashback);
-        Purchase p3 = new Purchase(60000, 30);
-        System.out.println(Cashback.cashback);
+        Purchase purchase1 = new Purchase(750, 5);
+        purchase1.getCashback();
+        Purchase purchase2 = new Purchase(4500, 1);
+        purchase2.getCashback();
+        Purchase purchase3 = new Purchase(60000, 30);
+        purchase3.getCashback();
     }
 }
