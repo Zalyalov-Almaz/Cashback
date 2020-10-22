d4da0f38e20e61a72fcd46b9d65b98258e5700d3
Алмаз Залялов <zalyalov.almaz2016@gmail.com>
2020-10-22 14:11:31 +0300

    fixed #1

--- src/dz2/Purchase.java
+++ src/dz2/Purchase.java
@@ -3,21 +3,21 @@ package dz2;
 public class Purchase {
     int summ;
     private int cashbackPercent;
-    private static int cashback;
+    private int cashback;
 
     public Purchase(int summ, int cashbackPercent) {
         this.summ = summ;
         if (cashbackPercent == 1 || cashbackPercent == 5 || cashbackPercent == 30) {
             this.cashbackPercent = cashbackPercent;
         }
-        cashback += this.summ * this.cashbackPercent/100;
+        this.cashback += this.summ * this.cashbackPercent/100;
         if (cashback >= 3000) {
             cashback = 3000;
         }
     }
 
-    public static int getCashback() {
-        System.out.println(cashback);
-        return cashback;
+    public int getCashback() {
+        System.out.println(this.cashback);
+        return this.cashback;
     }
 }
